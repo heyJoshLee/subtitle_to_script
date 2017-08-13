bd96247807b53cd7a4ba17c7c8015ae03e73c5a3
Josh Lee <lee45276@gmail.com>
2017-08-13 23:13:44 +0800

    Complete main feature

--- script.rb
+++ script.rb
@@ -1,32 +1,48 @@
 require "pry"
 require 'fileutils'
 
-class Subtitle
+class SubtitleFormatter
+  attr_accessor :file_paths
+
+  def initialize
+    get_files
+    process_files
+  end
 
 
-  def self.convert_subtitle_file_to_text(file)
+  def get_files
+    @file_paths = Dir["input/*/*"]
+  end
 
-    file_name = file.path.split("/").last.split(".").first
+  def process_files
+    @file_paths.each do |file_path|
+      convert_subtitle_file_to_text(file_path)
+    end 
+  end
+
+
+  def convert_subtitle_file_to_text(file_path)
+    original_file = File.open(file_path)
+    file_name = file_path.split("/").last.split(".").first
+    file_name += "-script.txt"
+
     output_contents = ""
 
-    file.each_line do |line|
+    original_file.each_line do |line|
       first_letter = line[0]
       next if !!(first_letter =~ /\A[-+]?[0-9]+\z/) || first_letter == "0"
       next if line == "\r\n"
-      binding.pry
       output_contents += line
     end
-    output_file = File.new(file_name + ".txt", "w:UTF-16LE")
+    new_folder_path = file_path.split("/")[1] + "/"
+    full_dir_path = "output/" + new_folder_path
+    Dir.mkdir(full_dir_path) unless File.directory?(full_dir_path)
+    output_file = File.new(full_dir_path + file_name, "w:UTF-8")
     output_file.puts(output_contents)
     output_file.close
     output_file
   end
 
-  def self.get_new_file_name(file)
-    file.path.split("/").last.split(".").first + ".txt"
-  end
-
-
 end
 
-binding.pry
+SubtitleFormatter.new
